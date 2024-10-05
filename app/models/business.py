03ab7828356e1b9b86dc3bc60518515f8af20f0e
Vitaliy <v.izhakivskiy@gmail.com>
2024-10-05 21:45:20 +0300

    user model

--- app/models/business.py
+++ app/models/business.py
@@ -7,5 +7,5 @@ class Business(Base):
     __tablename__ = 'business'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    code = Column(String(12), nullable=False)
+    code = Column(String(12), nullable=False, index=True)
     name = Column(String(255), nullable=False)
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 from app.db import Base
 
@@ -7,9 +9,9 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    first_name = Column(String(32), nullable=True)
-    last_name = Column(String(32), nullable=True)
     phone = Column(String(32), nullable=False, unique=True, index=True)
+    is_admin = Column(Boolean, default=False)
+    creation_time = Column(DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<User(id={self.id}, phone='{self.phone}')>"
+        return f"<User(id={self.id}, phone='{self.phone}', is_admin='{self.is_admin}')>"
